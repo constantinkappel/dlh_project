9920c04b13f7c4cc8805d0430cb728c70e6b618b
constantinkappel <constantinkappel@users.noreply.github.com>
2024-04-14 13:40:48 +0200

    Custom prerpocess_run.sh -> Do edit the path to preprocess_main.py !

---
+++ project_code/00_preprocess/preprocess_run.sh
@@ -0,0 +1,41 @@
+INPUT_PATH=/home/data
+OUTPUT_PATH=/home/data/output
+DX_PATH=$INPUT_PATH/ccs_multi_dx_tool_2015.csv
+
+python ../preprocess/preprocess_main.py \
+    --src_data mimiciii \
+    --dataset_path $INPUT_PATH/mimic \
+    --ccs_dx_tool_path $DX_PATH \
+    --dest_path $OUTPUT_PATH ;
+
+python ../preprocess/preprocess_main.py \
+    --src_data eicu \
+    --dataset_path $INPUT_PATH/eicu \
+    --ccs_dx_tool_path $DX_PATH \
+    --dest_path $OUTPUT_PATH ;
+
+python ../preprocess/preprocess_main.py \
+    --src_data pooled \
+    --ccs_dx_tool_path $DX_PATH \
+    --dest_path $OUTPUT_PATH ;
+
+
+python ../preprocess/preprocess_main.py \
+    --src_data mimiciii \
+    --dest_path $OUTPUT_PATH \
+    --ccs_dx_tool_path $DX_PATH \
+    --data_type pretrain ;
+
+python ../preprocess/preprocess_main.py \
+    --src_data eicu \
+    --dest_path $OUTPUT_PATH \
+    --ccs_dx_tool_path $DX_PATH \
+    --data_type pretrain ;
+
+python ../preprocess/preprocess_main.py \
+    --src_data pooled \
+    --dest_path $OUTPUT_PATH \
+    --ccs_dx_tool_path $DX_PATH \
+    --data_type pretrain ;
+
+
