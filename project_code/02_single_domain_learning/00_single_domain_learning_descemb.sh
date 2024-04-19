774b34ca68c7335dbc7d9c2f9872f10bd2af71d3
constantinkappel <constantinkappel@users.noreply.github.com>
2024-04-19 23:31:24 +0200

    Modified scripts for single domain learning

--- project_code/02_single_domain_learning/00_single_domain_learning_descemb.sh
+++ project_code/02_single_domain_learning/00_single_domain_learning_descemb.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 # Run single domain learning with descemb
-root='../../'
+INPUT_PATH=/data/DescEmb/output
+SRC_DATA=('mimiciii' 'eicu')
+root='../DescEmb/'
 embed_models=('descemb_rnn' 'descemb_bert')
+MODEL_PATH=('../DescEmb/outputs/2024-04-19/15-55-40' '../DescEmb/outputs/2024-04-19/06-18-04')
 tasks=('readmission' 'mortality' 'los_3day' 'los_7day' 'diagnosis')
 value_modes=('NV' 'VA' 'DSVA' 'DSVA_DPE' 'VC')
 
-for emb_model in "${embed_models[@]}"; do
-    echo "Processing model: $emb_model"
-    for task in "${tasks[@]}"; do
-        for value_mode in "${value_modes[@]}"; do
-            CUDA_VISIBLE_DEVICES=0 python ${root}main.py \
-                --distributed_world_size 1 \
-                --input_path '/data/DescEmb/output_pretrain'\
-                --model ehr_model \
-                --embed_model "$emb_model" \
-                --pred_model rnn \
-                --src_data mimiciii \
-                --ratio 100 \
-                --value_mode "$value_mode" \
-                --task "$task" ;
+
+for data in "${SRC_DATA[@]}"; do
+    for index in "${!embed_models[@]}"; do
+        emb_model=${embed_models[$index]}
+        model_path=${MODEL_PATH[$index]}
+        echo "Processing model: $emb_model with data $data"
+        for task in "${tasks[@]}"; do
+            for value_mode in "${value_modes[@]}"; do
+                CUDA_VISIBLE_DEVICES=0 python ${root}main.py \
+                    --distributed_world_size 1 \
+                    --input_path "$INPUT_PATH" \
+                    --model_path "$model_path" \
+                    --model ehr_model \
+                    --embed_model "$emb_model" \
+                    --pred_model rnn \
+                    --src_data $data \
+                    --ratio 100 \
+                    --value_mode "$value_mode" \
+                    --task "$task" ;
+            done
         done
     done
 done
--- project_code/02_single_domain_learning/01_single_domain_learning_codeemb.sh
+++ project_code/02_single_domain_learning/01_single_domain_learning_codeemb.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
 # Run single domain learning with codeemb
-root='../../'
+root='../DescEmb/'
+INPUT_PATH=/data/DescEmb/output
 embed_models=('codeemb')
 tasks=('readmission' 'mortality' 'los_3day' 'los_7day' 'diagnosis')
 value_modes=('NV' 'VA' 'DSVA' 'VC')
+SRC_DATA=('mimiciii' 'eicu')
+MODEL_PATH=('/../DescEmb/outputs/') # PLEASE MODIFY
 
-for emb_model in "${embed_models[@]}"; do
-    echo "Processing model: $emb_model"
-    for task in "${tasks[@]}"; do
-        for value_mode in "${value_modes[@]}"; do
-            CUDA_VISIBLE_DEVICES=0 python ${root}main.py \
-                --distributed_world_size 1 \
-                --input_path '/data/DescEmb/output_pretrain'\
-                --model ehr_model \
-                --embed_model "$emb_model" \
-                --pred_model rnn \
-                --src_data mimiciii \
-                --ratio 100 \
-                --value_mode "$value_mode" \
-                --task "$task" ;
+
+for data in "${SRC_DATA[@]}"; do
+    for index in "${!embed_models[@]}"; do
+        echo "Processing model: $emb_model with data $data"
+        for task in "${tasks[@]}"; do
+            for value_mode in "${value_modes[@]}"; do
+                CUDA_VISIBLE_DEVICES=0 python ${root}main.py \
+                    --distributed_world_size 1 \
+                    --input_path "$INPUT_PATH" \
+                    --model_path "${MODEL_PATH[$index]}" \
+                    --load_pretrained_weights \
+                    --model ehr_model \
+                    --embed_model "${emb_models[$index]}" \
+                    --pred_model rnn \
+                    --src_data $data \
+                    --ratio 100 \
+                    --value_mode "$value_mode" \
+                    --task "$task" ;
+            done
         done
     done
 done
-
