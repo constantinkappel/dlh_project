86b655728ad97a373b525af1a8b868ec9847c424
constantinkappel <constantinkappel@users.noreply.github.com>
2024-04-28 01:53:33 +0200

    Pre-training with FT-MLM

---
+++ project_code/01_pretraining/03_pretrain_descemb_bert_ft-mlm.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Run training new model using FT with descemb
+# run this from project root folder using
+# ./project_code/01_pretraining/03_pretrain_descemb_bert_ft-mlm.sh $(pwd)/project_code
+
+if [ -n "$1" ]; then
+    root="${1%/}/DescEmb/"
+else
+    root='../DescEmb/'
+fi 
+
+INPUT_PATH=/home/data/output # /data/DescEmb/output
+SRC_DATA=('mimiciii' 'eicu')
+MODEL='descemb_bert'
+TASK='mlm'
+for src_data in "${SRC_DATA}"; do
+    echo "Processing dataset: $src_data"
+    # Pre-train Descemb-BERT initialized with BERT params using MLM task 
+    CUDA_VISIBLE_DEVICES=0 python ${root}main.py \
+        --distributed_world_size 1 \
+        --input_path "$INPUT_PATH" \
+        --model "$MODEL" \
+        --init_bert_params \
+        --src_data "$src_data" \
+        --ratio 100 \
+        --value_mode NV \
+        --save_prefix "checkpoint_${src_data}_${MODEL}_ft-${TASK}" \
+        --patience 200 \
+        --task "$TASK" ;
+done
