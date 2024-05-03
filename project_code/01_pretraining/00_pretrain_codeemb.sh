7145eea437be84bb8f305fcfef97025ac9e3e5f0
constantinkappel <constantinkappel@users.noreply.github.com>
2024-05-04 00:55:47 +0200

    codeemb pretraining and training

--- project_code/01_pretraining/00_pretrain_codeemb.sh
+++ project_code/01_pretraining/00_pretrain_codeemb.sh
@@ -1,25 +1,31 @@
-root='../../'
-# W2V training 
-CUDA_VISIBLE_DEVICES=0 python ${root}main.py \
-    --distributed_world_size 1 \
-    --input_path '/data/DescEmb/output_pretrain' \
-    --model ehr_model \
-    --embed_model codeemb \
-    --pred_model rnn \
-    --src_data mimiciii \
-    --ratio 100 \
-    --value_mode NV \
-    --task w2v ;
+#!/bin/bash
+# Run pre-training with codeemb and W2V
+# run this from project root folder using
+# ./project_code/01_pretraining/03_pretrain_descemb_bert_ft-mlm.sh $(pwd)/project_code
 
+if [ -n "$1" ]; then
+    root="${1%/}/DescEmb/"
+else
+    root='../DescEmb/'
+fi 
 
-# W2V training 
-CUDA_VISIBLE_DEVICES=0 python ${root}main.py \
-    --distributed_world_size 1 \
-    --input_path '/data/DescEmb/output_pretrain' \
-    --model ehr_model \
-    --embed_model codeemb \
-    --pred_model rnn \
-    --src_data eicu \
-    --ratio 100 \
-    --value_mode NV \
-    --task w2v ;
+INPUT_PATH=/data/DescEmb/output #/home/data/output # /data/DescEmb/output
+SRC_DATA=('mimiciii' 'eicu')
+MODEL='codeemb'
+TASK='w2v'
+for src_data in "${SRC_DATA}"; do
+    echo "Processing dataset: $src_data"
+    # Pre-train Descemb-BERT initialized with BERT params using MLM task 
+    CUDA_VISIBLE_DEVICES=0 python ${root}main.py \
+        --distributed_world_size 1 \
+        --input_path "$INPUT_PATH" \
+        --model "$MODEL" \
+        --init_bert_params \
+        --src_data "$src_data" \
+        --ratio 100 \
+        --value_mode NV \
+        --save_prefix "checkpoint_${src_data}_${MODEL}_${TASK}" \
+        --patience 20 \
+        --task "$TASK" ;
+done
+
--- project_code/02_train_new_models/01_train_w2v_codeemb.sh
+++ project_code/02_train_new_models/01_train_w2v_codeemb.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
-# Run training new model using FT with descemb
+# Run training new model using W2V with codeemb
 # run this from project root folder using
-# ./project_code/02_train_new_models/03_train_ft_descemb_bert.sh $(pwd)/project_code
+# ./project_code/02_train_new_models/01_train_w2v_codeemb.sh $(pwd)/project_code
+
 
 if [ -n "$1" ]; then
     root="${1%/}/DescEmb/"
-    MODEL_PATH=("${1%/}/DescEmb/outputs/2024-04-19/06-18-04/checkpoints/checkpoint_mimiciii_descemb_bert_mlm_last.pt")
+    MODEL_PATH=("${1%/}/DescEmb/outputs/2024-05-04/07-33-59/checkpoints/checkpoint_mimiciii_codeemb_ft-w2v_best.pt")
 else
     root='../DescEmb/'
-    MODEL_PATH=('../../../outputs/2024-04-19/06-18-04/checkpoints/checkpoint_mimiciii_descemb_bert_mlm_last.pt')
+    MODEL_PATH=('../../../outputs/2024-05-04/07-33-59/checkpoints/checkpoint_mimiciii_codeemb_ft-w2v_best.pt')
 fi 
 
-INPUT_PATH=/home/data/output #/data/DescEmb/output
-SRC_DATA=('mimiciii' 'eicu')
+INPUT_PATH=/data/DescEmb/output #/home/data/output #/data/DescEmb/output
+SRC_DATA=('mimiciii') # 'eicu')
 
-embed_models=('descemb_bert')
+embed_models=('codeemb')
 tasks=('diagnosis' 'los_3day' 'los_7day' 'readmission' 'mortality')
-value_modes=('VA' 'DSVA' 'DSVA_DPE' 'VC')
+value_modes=('VA' 'DSVA' 'VC')
 
 
 for data in "${SRC_DATA[@]}"; do
@@ -30,13 +31,12 @@ for data in "${SRC_DATA[@]}"; do
                 CUDA_VISIBLE_DEVICES=0 python ${root}main.py \
                     --distributed_world_size 1 \
                     --input_path "$INPUT_PATH" \
-                    --init_bert_params \
                     --model ehr_model \
                     --embed_model "$emb_model" \
                     --pred_model rnn \
                     --src_data $data \
                     --ratio 100 \
-                    --patience 90 \
+                    --patience 45 \
                     --value_mode "$value_mode" \
                     --save_prefix "checkpoint_${data}_${emb_model}_${task}" \
                     --task "$task" ;
